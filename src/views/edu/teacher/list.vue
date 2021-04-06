2ae6963f6bae5ff92e472965880d09536129e8f7
andochiwa <a1066079469@gmail.com>
2021-04-06 23:53:19 +0900

    update pageInation

--- src/views/edu/teacher/list.vue
+++ src/views/edu/teacher/list.vue
@@ -1,32 +1,41 @@
 <template>
   <div class="app-container">
-    教师列表
+    <h1>教师列表</h1>
 
     <el-table :data="list" border style="width: 100%">
-
       <el-table-column label="序号" width="70">
         <template slot-scope="scope">
-          {{(page - 1) * limit + scope.$index + 1}}
+          {{ (page - 1) * limit + scope.$index + 1 }}
         </template>
       </el-table-column>
 
-      <el-table-column prop="name" label="姓名" width="80"/>
+      <el-table-column prop="name" label="姓名" width="80" />
 
-      <el-table-column label="头衔" width="80"> 
+      <el-table-column label="头衔" width="80">
         <template slot-scope="scope">
-          {{scope.row.level === 1 ? '高级教师' : '首席教师'}}
+          {{ scope.row.level === 1 ? "高级教师" : "首席教师" }}
         </template>
       </el-table-column>
 
-      <el-table-column prop="gmtCreate" label="创建时间" width="80"/>
+      <el-table-column prop="gmtCreate" label="创建时间" width="80" />
 
-      <el-table-column prop="gmtModified" label="修改时间" width="80"/>
+      <el-table-column prop="gmtModified" label="修改时间" width="80" />
 
-      <el-table-column prop="sort" label="排序" width="60"/>
+      <el-table-column prop="sort" label="排序" width="60" />
 
       <el-table-column prop="intro" label="资历"> </el-table-column>
-
     </el-table>
+
+    <el-pagination 
+      background
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getList"
+    >
+    </el-pagination>
   </div>
 </template>
 
@@ -48,7 +57,8 @@ export default {
   },
   methods: {
     // 讲师列表的方法
-    getList() {
+    getList(page = 1) {
+      this.page = page
       teacher
         .getTeacherListPage(this.page, this.limit, this.teacherQuery)
         .then((result) => {
