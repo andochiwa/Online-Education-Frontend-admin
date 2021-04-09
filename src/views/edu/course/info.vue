ee4a6d24305358f8274d74ecf93f4533884eeb53
andochiwa <a1066079469@gmail.com>
2021-04-09 21:49:09 +0900

    add course cover

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -3,7 +3,7 @@
 
     <h2 style="text-align: center">发布新课程</h2>
 
-    <el-steps :active="1" process-status="wait" align-center finish-status="success">
+    <el-steps :active="1" process-status="wait" finish-status="success">
       <el-step title="填写课程基本信息"></el-step>
       <el-step title="创建课程大纲"></el-step>
       <el-step title="发布课程"></el-step>
@@ -14,9 +14,15 @@
         <el-input v-model="courseInfo.title" placeholder="请输入课程标题"/>
       </el-form-item>
 
-      <!--  所属分类 TODO  -->
+      <el-form-item label="课程分类">
+        <el-cascader v-model="value"
+                     :options="subjectList"
+                     :props="defaultProps"
+                     @change="handleChange"
+                     placeholder="请选择课程分类">
+        </el-cascader>
+      </el-form-item>
 
-      <!--  课程教师 TODO  -->
       <el-form-item label="课程教师">
         <el-select v-model="courseInfo.teacherId" placeholder="请选择课程教师">
           <el-option
@@ -35,6 +41,17 @@
       </el-form-item>
 
       <!--  课程封面 TODO    -->
+      <el-form-item label="课程封面">
+        <el-upload
+          :action="BASE_API + '/edu-oss/file'"
+          :show-file-list="false"
+          :on-preview="beforeCoverUpload"
+          :on-success="handleCoverSuccess"
+          accept="image/*"
+          class="avatar-uploader">
+          <img :src="courseInfo.cover" width="100%" height="100%" alt=""/>
+        </el-upload>
+      </el-form-item>
 
       <el-form-item label="课程价格">
         <el-input-number :min="0" v-model="courseInfo.price" placeholder="请输入课程价格"/>
@@ -52,10 +69,12 @@
 
 <script>
 import course from '@/api/education/course'
+import subject from '@/api/education/subject'
 
 export default {
   data() {
     return {
+      BASE_API: process.env.VUE_APP_BASE_API + "/",
       disabledSaveButton: false,
       courseInfo: {
         title: '',
@@ -63,14 +82,22 @@ export default {
         subjectId: '',
         price: '',
         description: '',
-        cover: '',
+        cover: process.env.BASE_URL + '1.jpg',
         teacherId: ''
       },
-      teacherList: {}
+      teacherList: {},
+      subjectList: [],
+      defaultProps: {
+        value: 'id',
+        children: 'children',
+        label: 'title',
+        expandTrigger: 'hover'
+      }
     }
   },
   created() {
     this.getTeacher()
+    this.getSubject()
   },
   methods: {
     // 跳转到下一部
@@ -93,6 +120,40 @@ export default {
         .then(result => {
           this.teacherList = result.data.items
         })
+    },
+    // 查询课程分类
+    getSubject() {
+      subject.getSubjectList()
+        .then(result => {
+          this.subjectList = this.getTreeData(result.data.items)
+        })
+    },
+    // 递归判断，把最后一层children空数组置为undefined
+    getTreeData(data) {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].children.length === 0) {
+          data[i].children = undefined
+        } else {
+          this.getTreeData(data[i].children)
+        }
+      }
+      return data
+    },
+    // 课程分类改变时，把id绑定给courseInfo.subjectId
+    handleChange(data) {
+      this.courseInfo.subjectId = data[1]
+    },
+    // 上传封面之前
+    beforeCoverUpload(file) {
+      const isLt20M = file.size / 1024 / 1024 < 20
+      if (!isLt20M) {
+        this.$message.error("大小不能超过20M")
+      }
+      return isLt20M
+    },
+    // 上传封面成功后
+    handleCoverSuccess(result) {
+      this.courseInfo.cover = result.data.url
     }
   }
 }
